51779e1ec9cab9fc68bb6539dff92107ea5529f4
Jakub Pavlovic <jakubpavlovic47@seznam.cz>
2024-10-01 10:12:03 +0200

    add linkedin

--- constants/index.ts
+++ constants/index.ts
@@ -123,7 +123,7 @@ export const socialLinks = [
   {
     name: 'LinkedIn',
     iconUrl: linkedin,
-    link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    link: 'https://www.linkedin.com/in/jakub-pavlovic-33b765285/',
   },
 ];
 
